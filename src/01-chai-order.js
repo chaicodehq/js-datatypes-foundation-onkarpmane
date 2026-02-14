aff90fe99d9c877301916eba93035a612f07f31e
onkarpmane <onkarpmane27@gmail.com>
2026-02-15 01:27:42 +0530

    First order delivered successfully

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -47,20 +47,50 @@
  */
 export function getChaiOrderLength(order) {
   // Your code here
+  if(typeof order !== "string"){
+    return -1
+  }else{
+    return order.trim().length;
+  }
+  
 }
 
 export function shoutChaiOrder(order) {
   // Your code here
+  if(typeof order !== "string" || order.trim()==0){
+    return ""
+  }else{
+    return order.trim().toUpperCase();
+  }
 }
 
 export function whisperChaiOrder(order) {
   // Your code here
+  if(typeof order !== "string" || order.trim() == 0){
+    return ""
+  }else{
+    return order.trim().toLowerCase()
+  }
 }
 
 export function hasSpecialIngredient(order, ingredient) {
   // Your code here
+
+  if(typeof order!== "string"|| typeof ingredient !== "string"){
+    return false
+  }else{
+    return order.trim().toLowerCase().includes(ingredient.trim().toLowerCase())
+  }
 }
 
 export function getFirstAndLastChar(order) {
   // Your code here
+  
+  if(typeof order !=="string" || order.trim().length==0){
+    return null;
+  }else{
+    const first = order.trim().charAt(0);
+    const last = order.trim().at(-1)
+    return {first,last}
+  }
 }
